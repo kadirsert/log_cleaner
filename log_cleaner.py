8a971ef712ffac78ad6324c80185c2a20dadef80
Kadir Sert <kadirsert80@gmail.com>
2015-01-09 10:23:17 +0200

    props file changed, find -name ability added.

--- log_cleaner.py
+++ log_cleaner.py
@@ -24,29 +24,29 @@ try:
             if line == '' or line[0] == '#':
                 continue
             lc_props = line.split('|||')
-            if lc_props[2] == 'delete':
-                logger.info(lc_props[0] + ' pattern\'i kullanilarak ' + lc_props[1] + ' gunden bu yana degismemis dosyalar siliniyor!')
-                logger.info(os.popen('find ' + lc_props[0] + ' -mtime +' + lc_props[1]).read())
-                os.system('rm -f ' + '$(find ' + lc_props[0] + ' -mtime +' + lc_props[1] + ')')
-            if lc_props[2] == 'zip':
-                logger.info(lc_props[0] + ' pattern\'i kullanilarak ' + lc_props[1] + ' gunden bu yana degismemis dosyalar zipleniyor!')
-                log_files = os.popen('find ' + lc_props[0] + ' -mtime +' + lc_props[1]).read()
+            if lc_props[3] == 'delete':
+                logger.info(lc_props[0] + ' - ' + lc_props[1] + ' pattern\'i kullanilarak ' + lc_props[2] + ' gunden bu yana degismemis dosyalar siliniyor!')
+                logger.info(os.popen('find ' + lc_props[0] + ' -name ' + lc_props[1] + ' -mtime +' + lc_props[2]).read())
+                os.system('rm -f ' + '$(find ' + lc_props[0] + ' -name ' + lc_props[1] + ' -mtime +' + lc_props[2] + ')')
+            if lc_props[3] == 'zip':
+                logger.info(lc_props[0] + ' - ' + lc_props[1] + ' pattern\'i kullanilarak ' + lc_props[2] + ' gunden bu yana degismemis dosyalar zipleniyor!')
+                log_files = os.popen('find ' + lc_props[0] + ' -name ' + lc_props[1] + ' -mtime +' + lc_props[2]).read()
                 logger.info(log_files)
                 if log_files == '':
-                    logger.info(lc_props[0] + ' pattern\'i kullanilarak log dosyasi bulunamadi!')
+                    logger.info(lc_props[0] + ' - ' + lc_props[1] + ' pattern\'i kullanilarak log dosyasi bulunamadi!')
                     continue
                 if os.path.isfile(os.path.join(__location__, 'temp_file')):
                     os.remove(os.path.join(__location__, 'temp_file'))
                 temp_file = open(os.path.join(__location__, 'temp_file'), 'w')
                 temp_file.write(log_files)
                 temp_file.close()
-                tar_file = os.path.join(os.path.dirname(lc_props[3]), script_run_time + '_' + os.path.basename(lc_props[3]))
+                tar_file = os.path.join(os.path.dirname(lc_props[4]), script_run_time + '_' + os.path.basename(lc_props[4]))
                 os.system('tar -czvf ' + tar_file + ' -T ' + os.path.join(__location__, 'temp_file'))
                 if os.path.isfile(os.path.join(__location__, 'temp_file')):
                     os.remove(os.path.join(__location__, 'temp_file'))
-                logger.info(lc_props[0] + ' pattern\'i kullanilarak ' + lc_props[1] + ' gunden bu yana degismemis dosyalar siliniyor!')
-                logger.info(os.popen('find ' + lc_props[0] + ' -mtime +' + lc_props[1]).read())
-                os.system('rm -f ' + '$(find ' + lc_props[0] + ' -mtime +' + lc_props[1] + ')')
+                logger.info(lc_props[0] + ' - ' + lc_props[1] + ' pattern\'i kullanilarak ' + lc_props[2] + ' gunden bu yana degismemis dosyalar siliniyor!')
+                logger.info(os.popen('find ' + lc_props[0] + ' -name ' + lc_props[1] + ' -mtime +' + lc_props[2]).read())
+                os.system('rm -f ' + '$(find ' + lc_props[0] + ' -name ' + lc_props[1] + ' -mtime +' + lc_props[2] + ')')
         except Exception, e:
             logger.error(e)
 finally:
