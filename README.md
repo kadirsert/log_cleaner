053c6d00b276534cb2771f22efbec29efec7c080
Kadir Sert <kadirsert80@gmail.com>
2015-06-22 11:49:50 +0300

    Testing feature added.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # log_cleaner
-Simple python script for cleaning log files
+
+Simple python script for cleaning log files. Actually it implements linux find command using log_cleaner_properties file.
+For only testing the properties file and not applying the rules, just run as "./log_cleaner.py test"
+
+===========================
+log_cleaner_properties File
+===========================
+Every line of the rules should be as next line:
+Pattern of log files path ||| Pattern of log files ||| Number of days (log files those are unchanged since the given number of days. This corresponds to '-mtime +' option) ||| Type of operation (delete or zip) ||| Path of compressed log file will be saved (This is used only with zip option)
+
+=====================
+Sample Configurations
+=====================
+/some/folder/pattern_*|||"*.trc"|||20|||delete
+
+/other/folder/pattern_*|||"*.trc"|||20|||zip|||/other/folder/archives/TEST_traces.tar.gz
+/other/folder/archives|||"*_*_TEST_traces.tar.gz"|||20|||delete
--- log_cleaner.py
+++ log_cleaner.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python
 import os
+import sys
 from time import gmtime, strftime
 import logging
 
 __author__ = 'Kadir Sert'
 __location__ = os.path.dirname(os.path.abspath(__file__))
 script_run_time = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
+if len(sys.argv) == 1:
+    is_test_mode = False
+elif len(sys.argv) == 2 and sys.argv[1] == 'test':
+    is_test_mode = True
+    if os.path.isfile(os.path.join(__location__, 'log_cleaner_test.out')):
+        os.remove(os.path.join(__location__, 'log_cleaner_test.out'))
+else:
+    print 'Gecersiz Arguman !!!'
+    sys.exit()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler(os.path.join(__location__, 'log_cleaner.out'))
+if is_test_mode:
+    handler = logging.FileHandler(os.path.join(__location__, 'log_cleaner_test.out'))
+else:
+    handler = logging.FileHandler(os.path.join(__location__, 'log_cleaner.out'))
 handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
@@ -33,7 +46,8 @@ try:
                 logger.info(lc_message)
                 lc_command = 'rm -f ' + '$(find ' + lc_props[0] + ' -name ' + lc_props[1] + \
                     ' -mtime +' + lc_props[2] + ' -type f' + ')'
-                os.system(lc_command)
+                if not is_test_mode:
+                    os.system(lc_command)
             if lc_props[3] == 'zip':
                 lc_message = lc_props[0] + ' - ' + lc_props[1] + ' patternli ' + \
                     lc_props[2] + ' gundur degismemis dosyalar zipleniyor!'
@@ -53,7 +67,8 @@ try:
                 tar_file = os.path.join(os.path.dirname(lc_props[4]),
                                         script_run_time + '_' + os.path.basename(lc_props[4]))
                 lc_command = 'tar -czvf ' + tar_file + ' -T ' + os.path.join(__location__, 'temp_file')
-                os.system(lc_command)
+                if not is_test_mode:
+                    os.system(lc_command)
                 if os.path.isfile(os.path.join(__location__, 'temp_file')):
                     os.remove(os.path.join(__location__, 'temp_file'))
                 lc_message = lc_props[0] + ' - ' + lc_props[1] + ' patternli ' + \
@@ -64,7 +79,8 @@ try:
                 logger.info(lc_message)
                 lc_command = 'rm -f ' + '$(find ' + lc_props[0] + ' -name ' + lc_props[1] + \
                     ' -mtime +' + lc_props[2] + ' -type f' + ')'
-                os.system(lc_command)
+                if not is_test_mode:
+                    os.system(lc_command)
         except Exception, e:
             logger.error(e)
 finally:
