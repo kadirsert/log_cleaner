146171002ab593746dbe3738a1055f05626cc15f
Kadir Sert <kadirsert80@gmail.com>
2015-10-01 08:02:32 +0300

    log messages changed

--- log_cleaner.py
+++ log_cleaner.py
@@ -16,7 +16,7 @@ elif len(sys.argv) == 2 and sys.argv[1] == 'test':
     if os.path.isfile(os.path.join(__location__, 'log_cleaner_test.out')):
         os.remove(os.path.join(__location__, 'log_cleaner_test.out'))
 else:
-    print 'Gecersiz Arguman !!!'
+    print 'Invalid Argument !!!'
     sys.exit()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -29,7 +29,7 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.info('')
-logger.info('************************ Script Calismaya Basladi... ******************************')
+logger.info('************************ Script Started... ******************************')
 
 props_file = open(os.path.join(__location__, 'log_cleaner_properties'), 'r')
 for line in props_file:
@@ -39,7 +39,7 @@ for line in props_file:
             continue
         lc_props = line.split('|||')
         if lc_props[3] == 'delete':
-            lc_message = lc_props[0] + ' - ' + lc_props[1] + ' patternli ' + lc_props[2] + ' gundur degismemis dosyalar siliniyor!'
+            lc_message = 'Deleting file(s): ' + lc_props[0] + ' - ' + lc_props[1] + ' -mtime: ' + lc_props[2]
             logger.info(lc_message)
             lc_command = 'find ' + lc_props[0] + ' -name ' + lc_props[1] + ' -mtime +' + lc_props[2] + ' -type f'
             lc_message = os.popen(lc_command).read()
@@ -48,13 +48,13 @@ for line in props_file:
             if not is_test_mode:
                 os.system(lc_command)
         if lc_props[3] == 'zip':
-            lc_message = lc_props[0] + ' - ' + lc_props[1] + ' patternli ' + lc_props[2] + ' gundur degismemis dosyalar zipleniyor!'
+            lc_message = 'Compressing file(s): ' + lc_props[0] + ' - ' + lc_props[1] + ' -mtime: ' + lc_props[2]
             logger.info(lc_message)
             lc_command = 'find ' + lc_props[0] + ' -name ' + lc_props[1] + ' -mtime +' + lc_props[2] + ' -type f'
             log_files = os.popen(lc_command).read()
             logger.info(log_files)
             if log_files == '':
-                lc_message = lc_props[0] + ' - ' + lc_props[1] + ' patternli dosya bulunamadi!'
+                lc_message = lc_props[0] + ' - ' + lc_props[1] + ' file(s) not found!'
                 logger.info(lc_message)
                 continue
             if not is_test_mode:
@@ -69,7 +69,7 @@ for line in props_file:
                 os.system(lc_command)
                 if os.path.isfile(os.path.join(__location__, 'temp_file')):
                     os.remove(os.path.join(__location__, 'temp_file'))
-            lc_message = lc_props[0] + ' - ' + lc_props[1] + ' patternli ' + lc_props[2] + ' gundur degismemis dosyalar siliniyor!'
+            lc_message = 'Deleting file(s): ' + lc_props[0] + ' - ' + lc_props[1] + ' -mtime: ' + lc_props[2]
             logger.info(lc_message)
             lc_command = 'find ' + lc_props[0] + ' -name ' + lc_props[1] + ' -mtime +' + lc_props[2] + ' -type f'
             lc_message = os.popen(lc_command).read()
